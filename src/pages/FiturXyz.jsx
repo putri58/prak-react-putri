94b6d9efd7c712ee2fa8a6a2dc31cbcca990cd4b
putri58 <putri24si@mahasiswa.pcr.ac.id>
2026-06-05 15:45:10 +0700

    penambahan shadcnui

--- src/pages/FiturXyz.jsx
+++ src/pages/FiturXyz.jsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 export default function FiturXyz() {
   return (
     <div id="dashboard-container" className="p-6">
@@ -23,7 +36,63 @@ export default function FiturXyz() {
       {/* Content */}
       <div>
         <p className="text-gray-700">
-          Ini Adalah Halaman Fitur XYZ
+        
+
+          <Button>TEST</Button>
+          <Button variant="outline" size="xs">Outline</Button>
+          <Button variant="default" size="lg" >Default</Button>
+          <Button variant="destructive">Destructive</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="ghost">Ghost</Button>
+          <Button variant="link">Link</Button>
+
+
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+        <CardAction>
+          <Button variant="link">Sign Up</Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+      </CardFooter>
+    </Card>
         </p>
       </div>
     </div>
